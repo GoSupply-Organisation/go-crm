5d0ceb5dd2aa77358345c5f41b8c5fdcf961b891
Zeezus426 <zachaldin@outlook.com>
2025-11-07 11:02:05 +1100

    added the views, urls and migrations to views, added the url paths to core, for superresearcher and contacts going to continue with the project

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404   
+from .models import Contact
 # Create your views here.
+
+
+def contact_list(request):
+    contacts = Contact.objects.all()
+    return render(request, 'contacts/contact_list.html', {'contacts': contacts})
+
+
+def contact_create(request):
+    if request.method == "POST":
+        contact = Contact(
+        first_name = request.POST.get['first_name'],
+        last_name = request.POST.get['last_name'],
+        email = request.POST.get['email'],
+        phone = request.POST.get['phone'])
+        contact.save
+        return redirect('contact_list')
+    return render(request, 'contacts/contact_form.html')
+
+
+def contact_update(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    if request.method == "POST":
+        contact.first_name = request.POST.get['first_name']
+        contact.last_name = request.POST.get['last_name']
+        contact.email = request.POST.get['email']
+        contact.phone = request.POST.get['phone']
+        contact.save()
+        return redirect('contact_list')
+    return render(request, 'contacts/contact_form.html', {'contact': contact})
