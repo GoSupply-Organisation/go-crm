6c0619383fb49cbb5a83575fc92ea9b2b7cc9ba4
Zeezus426 <zachaldin@outlook.com>
2025-11-18 16:30:58 +1100

    just some stuff thats breaking gonna apply claude changes to see if it works

--- super_researcher/Smol_n_multi.py
+++ super_researcher/Smol_n_multi.py
@@ -6,12 +6,13 @@ from IPython.display import Image, display
 from pydantic import BaseModel, Field
 from typing import Annotated, List
 import operator
-from langchain.messages import HumanMessage, SystemMessage
+from langchain.messages import HumanMessage, SystemMessage, ToolMessage
 from langchain_openai import ChatOpenAI
 from langgraph.types import Send
 from langchain_mcp_adapters.client import MultiServerMCPClient  
 from langchain.messages import AnyMessage
-
+from langgraph.graph import MessagesState
+from typing_extensions import Literal
 
 client = MultiServerMCPClient({  
         "search": {
@@ -102,6 +103,29 @@ async def main():
 
         # Write the updated section to completed sections
         return {"completed_sections": [section.content]}
+    
+    def tool_node(state: dict):
+        """Performs the tool call"""
+
+        result = []
+        for tool_call in state["messages"][-1].tool_calls:
+            tool = tools_by_name[tool_call["name"]]
+            observation = tool.invoke(tool_call["args"])
+            result.append(ToolMessage(content=observation, tool_call_id=tool_call["id"]))
+        return {"messages": result}
+    
+    def should_continue(state: MessagesState) -> Literal["tool_node", END]:
+        """Decide if we should continue the loop or stop based upon whether the LLM made a tool call"""
+
+        messages = state["messages"]
+        last_message = messages[-1]
+
+        # If the LLM makes a tool call, then perform an action
+        if last_message.tool_calls:
+            return "tool_node"
+
+        # Otherwise, we stop (reply to the user)
+        return END
 
 
     def synthesizer(state: State):
@@ -131,14 +155,18 @@ async def main():
     # Add the nodes
     orchestrator_worker_builder.add_node("orchestrator", orchestrator)
     orchestrator_worker_builder.add_node("llm_call", llm_call)
+    orchestrator_worker_builder.add_node("tool_node", tool_node)
     orchestrator_worker_builder.add_node("synthesizer", synthesizer)
 
     # Add edges to connect nodes
     orchestrator_worker_builder.add_edge(START, "orchestrator")
+    
     orchestrator_worker_builder.add_conditional_edges(
-        "orchestrator", assign_workers, ["llm_call"]
+        "orchestrator", 
+        assign_workers, 
+        ["llm_call"], should_continue, ["tool_node"]
     )
-    orchestrator_worker_builder.add_edge("llm_call", "synthesizer")
+    orchestrator_worker_builder.add_edge(["llm_call"], "synthesizer")
     orchestrator_worker_builder.add_edge("synthesizer", END)
 
     # Compile the workflow
