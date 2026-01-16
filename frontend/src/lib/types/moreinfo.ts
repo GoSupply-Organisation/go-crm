e4c22bb740668005ca72f4203b777cea8253aa1e
Zeezus426 <166266991+Zeezus426@users.noreply.github.com>
2026-01-16 12:07:42 +1100

    saving the new code for more info need to fix the email buttons

---
+++ frontend/src/lib/types/moreinfo.ts
@@ -0,0 +1,12 @@
+export type MoreInfoResponse = {
+    id: number;
+    Full_name: string;
+    email: string;
+    phone_number: string;
+    company: string;
+    lead_class: string;
+    notes: string;
+    address: string;
+    created_at: string;
+
+};
