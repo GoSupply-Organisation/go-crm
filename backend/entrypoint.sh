c669365b9dd0d25d5633ea939e96583211b13550
YakPort <35947849+YakPort@users.noreply.github.com>
2026-01-23 13:21:02 +1100

    removed collectstatic

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "Collecting static files to Azure Storage..."
-python manage.py collectstatic --noinput
+# echo "Collecting static files to Azure Storage..."
+# python manage.py collectstatic --noinput
 python manage.py migrate
 echo "Starting Daphne ASGI server..."
 exec daphne -b 0.0.0.0 -p 80 --proxy-headers core.asgi:application
